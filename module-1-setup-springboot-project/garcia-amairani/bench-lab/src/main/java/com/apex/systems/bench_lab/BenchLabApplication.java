eecf6bf53a7a1f498f5890cc4e553e88f80d69f3
amgarcia <amgarcia@apexsystems.com>
2025-02-15 02:10:27 -0600

    add new directories and update gitignore

---
+++ module-1-setup-springboot-project/garcia-amairani/bench-lab/src/main/java/com/apex/systems/bench_lab/BenchLabApplication.java
@@ -0,0 +1,13 @@
+package com.apex.systems.bench_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchLabApplication.class, args);
+	}
+
+}
